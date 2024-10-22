718a419b56a793d71dbd839d82838a6bdf2946c3
Haneol <haneolj@gmail.com>
2024-10-22 15:05:48 +0900

    feat: 포스팅 카드 구현 중

--- src/components/PostingCard.jsx
+++ src/components/PostingCard.jsx
@@ -1,7 +1,91 @@
 import React from "react";
+import styled from "styled-components";
+import theme from "../theme";
 
-function PostingCard() {
-  return <div>PostingCard</div>;
+const StyledPosting = styled.div`
+  width: 330px;
+  height: 450px;
+  border-radius: ${theme.borderRadius.md};
+  background: ${theme.colors.white};
+`;
+
+const ImageContainer = styled.div`
+  width: 100%;
+  padding-top: 56.25%;
+  position: relative;
+`;
+
+const StyledImg = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: ${theme.borderRadius.md};
+  object-fit: cover;
+`;
+
+const ContentContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 10px 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Title = styled.h3`
+  font-size: ${theme.fontSizes.lg};
+  font-weight: ${theme.fontWeight.bold};
+  color: ${theme.colors.neutral700};
+`;
+
+const Desciption = styled.p`
+  font-size: ${theme.fontSizes.md};
+  font-weight: ${theme.fontWeight.light};
+  color: ${theme.colors.neutral700};
+  padding: 10px 0;
+`;
+
+const PostingInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+function PostingCard({
+  postingId,
+  title,
+  travelDate,
+  mainImg,
+  content,
+  nickname,
+  profileImg,
+  createAt,
+}) {
+  return (
+    <StyledPosting>
+      <ImageContainer>
+        <StyledImg
+          src={mainImg}
+          alt="main image"
+          loading="lazy"
+          decoding="async"
+          onLoad={(e) => {}}
+        />
+      </ImageContainer>
+      <ContentContainer>
+        <div>
+          <Title>{title}</Title>
+          <Desciption>{content}</Desciption>
+        </div>
+        <div>
+          <PostingInfo>
+            
+          </PostingInfo>
+        </div>
+      </ContentContainer>
+    </StyledPosting>
+  );
 }
 
 export default PostingCard;
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Button from "../../components/Button";
 import EditButton from "../../components/EditButton";
 import Input from "../../components/Input";
+import PostingCard from "../../components/PostingCard";
+import theme from "../../theme";
 
 function Main() {
   const handleClick = () => {
@@ -9,7 +11,7 @@ function Main() {
   };
 
   return (
-    <div>
+    <div style={{ backgroundColor: theme.colors.neutral50, height: "100vh" }}>
       Main
       <br></br>
       <Button
@@ -22,6 +24,18 @@ function Main() {
       <EditButton btnClick={handleClick} width={150} text="수정" />
       <br></br>
       <Input type="searc" placeholder="검색어를 입력해주세여." />
+      <PostingCard
+        postingId={1}
+        title={"오사카 여행일지 #4"}
+        travelDate={"2024-10-30"}
+        mainImg={"https://i.imgur.com/0TIs0vO.png"}
+        content={
+          "오른쪽 하단에 적혀있는 날짜는 실제 작성일이며, 여행 날짜와 다를 수 있습니다."
+        }
+        nickname={"USER001"}
+        profileImg={"https://i.imgur.com/T2tqUEG.png"}
+        createAt={"2024-10-30"}
+      />
     </div>
   );
 }
