d8928a938e83185a4737433f5784ce6c3c034a27
Haneol <haneolj@gmail.com>
2024-10-22 12:30:14 +0900

    feat: 테마 추가

---
+++ theme.js
@@ -0,0 +1,40 @@
+const theme = {
+  colors: {
+    primary: "#6A68F9",
+    primaryLight: "#9E9CFB",
+    primaryDark: "#4A48C9",
+    success: "#10B981",
+    warning: "#FBBF24",
+    error: "#EF4444",
+    info: "#3B82F6",
+    neutrall50: "#F8FAFC",
+    neutrall100: "#F1F5F9",
+    neutrall200: "#E2E8F0",
+    neutrall300: "#CBD5E1",
+    neutrall400: "#94A3B8",
+    neutrall500: "#64748B",
+    neutrall600: "#475569",
+    neutrall700: "#334155",
+    neutrall800: "#1E293B",
+    neutrall900: "#0F172A",
+    white: "#FFFFFF",
+    modalBackground: "#00000048",
+  },
+  fontSizes: {
+    xs: "10px",
+    sm: "13px",
+    md: "16px",
+    lg: "20px",
+    xl: "24px",
+    h4: "20px",
+    h3: "24px",
+    h2: "28px",
+    h1: "36px",
+  },
+  borderRadius: {
+    sm: "15px",
+    md: "30px",
+  },
+};
+
+export default theme;
